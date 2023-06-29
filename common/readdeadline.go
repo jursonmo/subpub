81fd945d75aa89307cb103d8385f5b611326f49e
william <myname@example.com>
2023-06-30 01:01:09 +0800

    fix:SetReadDeadline

--- common/readdeadline.go
+++ common/readdeadline.go
@@ -7,7 +7,10 @@ import (
 )
 
 func SetReadDeadline(conn *websocket.Conn, isServer bool, readDeadline time.Duration) {
-	wrapHanlder := func(handler func(string) error, readDeadline time.Duration) func(string) error {
+	//fix:先设置一个deadline, 如果没有收到ping or pong 自动超时, 收到ping or pong 后 会更新deadline
+	conn.SetReadDeadline(time.Now().Add(readDeadline)) //如果这里不设置deadline，可能一直收不到ping,那么永远都无法超时
+
+	wrapHanlderWithDeadline := func(handler func(string) error, readDeadline time.Duration) func(string) error {
 		return func(s string) error {
 			conn.SetReadDeadline(time.Now().Add(readDeadline))
 			if handler != nil {
@@ -20,9 +23,9 @@ func SetReadDeadline(conn *websocket.Conn, isServer bool, readDeadline time.Dura
 	var handler func(string) error
 	if isServer {
 		handler = conn.PingHandler()
-		conn.SetPingHandler(wrapHanlder(handler, readDeadline))
+		conn.SetPingHandler(wrapHanlderWithDeadline(handler, readDeadline))
 	} else {
 		handler = conn.PongHandler()
-		conn.SetPongHandler(wrapHanlder(handler, readDeadline))
+		conn.SetPongHandler(wrapHanlderWithDeadline(handler, readDeadline))
 	}
 }
